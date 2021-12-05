644c36009faeed99ad962f008c5b445cbc5dc1fc
octaviandascalu <75310083+octaviandascalu@users.noreply.github.com>
2021-12-05 18:30:11 +0200

    update: afisare timp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from crawler import Crawler
 from dbconnection import DbConnection
+
 # from esantionDateCrawler import stats
 
 start = time.time()
@@ -8,7 +9,7 @@ crawler = Crawler()
 crawler.start()
 crawler.stop()
 stop = time.time()
-print("Timp:", stop - start, "secunde")
+print("Timp:", (stop - start) // 60, "minute ", (stop - start) % 60, "secunde")
 
 db_conn = DbConnection()
 db_conn.createDatabase()
