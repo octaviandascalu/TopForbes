d653a5d7fbb0e1ce66fe42f14e4c638d0785b66e
octaviandascalu <75310083+octaviandascalu@users.noreply.github.com>
2021-12-05 23:05:20 +0200

    update: modificare afisare

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import time
 from crawler import Crawler
 from dbconnection import DbConnection
-
 # from esantionDateCrawler import stats
 
 start = time.time()
 crawler = Crawler()
 crawler.start()
 stop = time.time()
-print("Timp:", (stop - start) // 60, "minute ", (stop - start) % 60, "secunde")
-crawler.exportExtractedStats()
+print("Timp:", int(stop - start) // 60, "minute ", round(stop - start) % 60, "secunde\n")
 
 db_conn = DbConnection()
 db_conn.createDatabase()
 db_conn.insertIntoDatabase(crawler.exportExtractedStats())
 
-print("Top 10 tineri din Forbes:", db_conn.top10CeleMaiTineri())
-print("Top 10 filantropi din Forbes:", db_conn.top10Filantropi())
-print("In top 200 Forbes sunt", db_conn.cetatenieAmericana(True), "de cetateni americani.")
+print("Top 10 tineri din Forbes:")
+print(*db_conn.top10CeleMaiTineri(), sep=",")
+print()
+print("Top 10 filantropi din Forbes:")
+print(*db_conn.top10Filantropi(), sep=",")
+print()
+print("In top 200 Forbes sunt", db_conn.cetatenieAmericana(True), "de cetateni americani.\n")
